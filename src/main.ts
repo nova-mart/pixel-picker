c0b9384c113b9f2ef33268ff576226e09ae8e57d
nova-mart <nova-mart@outlook.com>
2026-05-12 09:28:30 -0400

    fix: eslinter errors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import {App, setIcon, Editor, MarkdownView, Modal, Notice, Plugin} from 'obsidian';
-import {DEFAULT_SETTINGS, MyPluginSettings, SampleSettingTab} from "./settings";
+import {DEFAULT_SETTINGS, PixelPickerSettings, PixelPickerSettingTab} from "./settings";
 
 // Remember to rename these classes and interfaces!
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PixelPicker extends Plugin {
+	settings: PixelPickerSettings;
 
 	async onload() {
 		await this.loadSettings();
@@ -13,7 +13,7 @@ export default class MyPlugin extends Plugin {
 		let isIconA = true;
         
         // 1. Add the initial icon
-        const ribbonIcon = this.addRibbonIcon('star', 'Toggle Icon', () => {
+        const ribbonIcon = this.addRibbonIcon('star', 'Toggle icon', () => {
             // 2. Modify icon on click
 			ribbonIcon.classList.toggle('my-plugin-icon-active', isIconA);
             if (isIconA) {
@@ -33,7 +33,7 @@ export default class MyPlugin extends Plugin {
 			id: 'open-modal-simple',
 			name: 'Open modal (simple)',
 			callback: () => {
-				new SampleModal(this.app).open();
+				new PixelPickerModal(this.app).open();
 			}
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
@@ -55,7 +55,7 @@ export default class MyPlugin extends Plugin {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
 					if (!checking) {
-						new SampleModal(this.app).open();
+						new PixelPickerModal(this.app).open();
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
@@ -66,7 +66,7 @@ export default class MyPlugin extends Plugin {
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new PixelPickerSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -74,16 +74,13 @@ export default class MyPlugin extends Plugin {
 			new Notice("Click");
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-
 	}
 
 	onunload() {
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<MyPluginSettings>);
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData() as Partial<PixelPickerSettings>);
 	}
 
 	async saveSettings() {
@@ -91,7 +88,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+class PixelPickerModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
--- src/settings.ts
+++ src/settings.ts
@@ -1,18 +1,18 @@
 import {App, PluginSettingTab, Setting} from "obsidian";
-import MyPlugin from "./main";
+import PixelPicker from "./main";
 
-export interface MyPluginSettings {
+export interface PixelPickerSettings {
 	mySetting: string;
 }
 
-export const DEFAULT_SETTINGS: MyPluginSettings = {
+export const DEFAULT_SETTINGS: PixelPickerSettings = {
 	mySetting: 'default'
 }
 
-export class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export class PixelPickerSettingTab extends PluginSettingTab {
+	plugin: PixelPicker;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PixelPicker) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
