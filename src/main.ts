badc9e36753cf55c26f203c79b69fc2b72c710a7
nova-mart <nova-mart@outlook.com>
2026-05-12 11:35:44 -0400

    refactor: remove extraneous sample plugin behaviours

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {App, Editor, MarkdownView, Modal, Notice, Plugin, Setting} from 'obsidian';
+import {App, Modal, Notice, Plugin, Setting} from 'obsidian';
 import {DEFAULT_SETTINGS, PixelPickerSettings, PixelPickerSettingTab} from "./settings";
 
 // Remember to rename these classes and interfaces!
@@ -15,10 +15,6 @@ export default class PixelPicker extends Plugin {
 			}).open();
         });
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status bar text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-modal-simple',
@@ -29,45 +25,9 @@ export default class PixelPicker extends Plugin {
 				}).open();
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'replace-selected',
-			name: 'Replace selected content',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				editor.replaceSelection('Sample editor command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-modal-complex',
-			name: 'Open modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new PixelPickerModal(this.app, (result) => {
-  							new Notice(`Hello, ${result}!`);
-						}).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-				return false;
-			}
-		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new PixelPickerSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', () => {
-			new Notice("Click");
-		});
 
 	}
 
